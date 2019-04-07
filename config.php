b0793d9ae72adc208d07dffeafe9105e2f23c5de
Winko Lui <winkoxd@icloud.com>
2019-04-07 20:53:08 +0800

    Init

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://59.148.6.210/302/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://59.148.6.210/302/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/302/catalog/');
+define('DIR_SYSTEM', '/var/www/html/302/system/');
+define('DIR_IMAGE', '/var/www/html/302/image/');
+define('DIR_LANGUAGE', '/var/www/html/302/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/302/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/302/system/config/');
+define('DIR_CACHE', '/var/www/html/302/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/302/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/302/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/302/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/302/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '302');
+define('DB_PASSWORD', '302');
+define('DB_DATABASE', '302');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '302e_');
